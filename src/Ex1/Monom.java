618311195d283aa149c98b0303b3a04d9e37372d
Itay Simhayev <itaysim7@gmail.com>
2019-11-26 21:25:12 +0200

    files from task0

---
+++ src/Ex1/Monom.java
@@ -0,0 +1,299 @@
+package Ex1;
+
+import java.util.Comparator;
+
+
+import javax.management.RuntimeErrorException;
+
+
+/**
+ * This class represents a simple "Monom" of shape a*x^b, where a is a real number and a is an integer (summed a none negative), 
+ * see: https://en.wikipedia.org/wiki/Monomial 
+ * The class implements function and support simple operations as: construction, value at x, derivative, add and multiply. 
+ * @author Boaz
+ *
+ */
+public class Monom implements function
+{
+	public static final Monom ZERO = new Monom(0,0);
+	public static final Monom MINUS1 = new Monom(-1,0);
+	public static final double EPSILON = 0.0000001;
+	public static final Comparator<Monom> _Comp = new Monom_Comperator();
+	private double _coefficient; 
+	private int _power;
+	
+	/**
+	 * @return the Comparator of this monom
+	 */
+	public static Comparator<Monom> getComp() 
+	{
+		return _Comp;
+	}
+
+	/**
+	 * init a Monom given double a and int b
+	 * a represent the coefficient
+	 * b represent the power
+	 * @param double a, int b
+	 */
+	public Monom(double a, int b)
+	{
+		if(a==0) 
+		{
+			this.set_coefficient(0);
+			this.set_power(0);
+		}
+		else
+		{
+			this.set_coefficient(a);
+			this.set_power(b);
+		}
+	}
+	/**
+	 * init a deep copy of the Monom ot
+	 * @param Monom ot
+	 */
+	public Monom(Monom ot) {
+		this(ot.get_coefficient(), ot.get_power());
+	}
+	/**
+	 * @return the coefficient
+	 */
+	public double get_coefficient() {
+		return this._coefficient;
+	}
+	/**
+	 * @return the power
+	 */
+	public int get_power() {
+		return this._power;
+	}
+	/** 
+	 * @return the derivative Monom of this.
+	 */
+	public Monom derivative()
+	{
+		if(this.get_power()==0) 
+		{
+			return getNewZeroMonom();
+		}
+		return new Monom(this.get_coefficient()*this.get_power(), this.get_power()-1);
+	}
+	/**
+	 * @return the value of this Monom in the given x
+	 * @param int x
+	 */
+	public double f(double x)/*hatzaba*/
+	{
+		double ans=0;
+		double p = this.get_power();
+		ans = this.get_coefficient()*Math.pow(x, p);
+		return ans;
+	} 
+	/**
+	 * 
+	 * @return if this is the Zero Monom
+	 */
+	public boolean isZero() 
+	{
+		return this.get_coefficient() == 0;
+	}
+	// ***************** add your code below **********************
+	
+	/**
+	 * init a Polynom from a String such as:
+	 * {"x","2x","x^2","3x^3","3"} 
+	 * @param s: is a string represents a Polynom
+	 */
+	public Monom(String s)
+	{
+		boolean conX=s.contains("x");
+		if(!conX) /*if the String isn't contains x,check if its legal double*/
+		{
+			boolean isD=s.matches("[-]?[0-9]*\\.?[0-9]*")&&!s.equals("-")&&!s.equals("")&&!s.equals(".");
+			if(!isD) 
+				throw new RuntimeException("ERR the string isnt legal ");
+			else
+			{ /*"Monom" of shape a*x^0(without x)*/
+					double c=Double.parseDouble(s);
+					this.set_coefficient(c);
+					this.set_power(0);
+			}
+		}
+		else
+		{ /*the string contains x*/
+			int xplace=s.indexOf('x');
+			String str1=s.substring(0, xplace);
+			boolean isD=str1.matches("[-]?[0-9]*\\.?[0-9]*");/*check if the coefficient is legal*/
+			boolean isI=true;
+			String str2="1";
+			if(xplace+1!=s.length()) /*if x is not the last char at the string*/
+			{
+				str2=s.substring(xplace+2,s.length());
+				isI=str2.matches("\\d+"); /*check if the power is legal*/
+			}
+			if((isD)&&(isI)&&((xplace+1==s.length()||s.charAt(xplace+1)=='^')))/*check if the string is from the shape ax^b*/
+			{
+				int p=Integer.parseInt(str2);
+				if(str1.equals("-")||str1.equals("")) //"Monom" of shape  -/+x^b
+				{
+					if(str1.equals("-")) //"Monom" of shape -x^b
+					{
+						this.set_coefficient(-1);
+						this.set_power(p);
+					}
+					else // "Monom" of shape x^b
+					{
+						this.set_coefficient(1);
+						this.set_power(p);
+					}
+				}
+				else
+				{
+					double c=Double.parseDouble(str1);
+					if(c==0)/* "Monom" of shape  0*/
+					{
+						this.set_coefficient(0);
+						this.set_power(0);
+					}
+					else
+					{ /*"Monom" of shape a*x^b*/
+						this.set_coefficient(c);
+						this.set_power(p);
+					}
+				}
+			}
+			else
+				throw new RuntimeException("ERR the string isnt legal ");
+		}
+	}
+	/**
+	 * Add Monom m to this Monom
+	 * @param Monom m
+	 */
+	public void add(Monom m)
+	{
+		if(m.get_coefficient()!=0)/*if m is not 0,else add 0*/
+		{
+			if(this._coefficient==0) /*if this Monom is 0 add m*/
+			{
+				this._coefficient=m.get_coefficient();
+				this._power=m.get_power();
+			}
+			else
+			{
+				if(m.get_power()!=this._power&&m.get_coefficient()!=0&&this._coefficient!=0) /*if the power of the Monom is different throw Exception*/
+				{
+					throw new RuntimeException("ERR the power of the Monoms must be the same");
+				}
+				else /*if the coefficient is the same set Monom to zero*/
+					if(m.get_coefficient()+this._coefficient==0) 
+					{
+						this._power=0;
+						this._coefficient=0;
+					}
+					else /*calculate the new Monom */
+						this._coefficient=this._coefficient+m.get_coefficient();
+			}
+		}
+	}
+	/**
+	 * Multiply this Monom by Monom p
+	 * @param Monom p
+	 */
+	public void multipy(Monom d) 
+	{
+		if(d.get_coefficient()==0||this._coefficient==0)/*if d is zero or this Monom is zero return 0*/
+		{
+			this._power=0;
+			this._coefficient=0;
+		}
+		else /*calculate the new Monom */
+		{
+			this._coefficient=this._coefficient*d.get_coefficient();
+			this._power=this._power+d.get_power();
+		}
+	}
+	/**
+	 * @return a string that represent this Monom
+	 * 
+	 */
+	public String toString()
+	{
+		String ans="";
+		if(this._coefficient==0)/*"Monom" of shape 0*/
+		{
+			return "0";
+		}
+		if(this._power==0)/*"Monom" of shape a*x^0*/
+		{
+			ans=""+this._coefficient;
+			return ans;
+		}
+		if(this._power==1)/*"Monom" of shape a*x^0*/
+		{
+			ans=""+this._coefficient+"x";
+			return ans;
+		}
+		/*"Monom" of shape ax^b*/
+		ans=""+this._coefficient+"x^"+this._power;
+		return ans;
+	}
+	/**
+	 * Test if this Monom is logically equals to Monom m.
+	 * @param Monom m
+	 * @return true if this Monom  represents the same function as Monom m
+	 */
+	public boolean equals(Monom m)
+	{
+		if(this._coefficient==m.get_coefficient()&&this._coefficient==0)
+			return true;
+		else
+		{
+			double e=Math.abs(this._coefficient-m.get_coefficient());
+			return (e<=EPSILON&&this._power==m.get_power());
+		}
+	}
+	// you may (always) add other methods.
+
+	//****************** Private Methods and Data *****************
+	
+	/**
+	 * set the Corfficient as a
+	 * @param double a
+	 */
+	private void set_coefficient(double a){
+		this._coefficient = a;
+	}
+	/**
+	 * set the power as p
+	 * @param int p
+	 */
+	private void set_power(int p) {
+		if(p<0) {throw new RuntimeException("ERR the power of Monom should not be negative, got: "+p);}
+		this._power = p;
+	}
+	/**
+	 * @return a new zero Monom
+	 */
+	private static Monom getNewZeroMonom() 
+	{
+		return new Monom(ZERO);
+	}
+
+	@Override
+	public function initFromString(String s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public function copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
+
+    
---
+++ src/Ex1/Polynom.java
@@ -0,0 +1,361 @@
+package Ex1;
+
+import java.util.ArrayList;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.function.Predicate;
+import Ex1.Monom;
+/**
+ * This class represents a Polynom with add, multiply functionality, it also should support the following:
+ * 1. Riemann's Integral: https://en.wikipedia.org/wiki/Riemann_integral
+ * 2. Finding a numerical value between two values (currently support root only f(x)=0).
+ * 3. Derivative
+ * 
+ * @author Boaz
+ *
+ */
+public class Polynom implements Polynom_able
+{
+	public static final Monom ZERO = new Monom(0,0);
+	public static final Monom MINUS1 = new Monom(-1,0);
+	private	ArrayList<Monom> poly ;
+	/**
+	 * Zero (empty polynom)
+	 */
+	public Polynom() 
+	{
+		poly=new ArrayList<Monom>() ;
+	}
+	/**
+	 * init a Polynom from a String consisting of a sequence of Monoms
+	 * @param s: is a string represents a Polynom
+	 */
+	public Polynom(String s) 
+	{
+		poly=new ArrayList<Monom>() ;
+		int i=0;
+		int start=i;
+		boolean minus=false;
+		while(i<s.length()) 
+		{
+			if(s.charAt(i)=='-'&&((i==0)||s.charAt(i-1)!='+'))//check if its negative 
+			{
+				minus=true;
+				i++;
+			}
+			start=i;
+			while(i<s.length()&&s.charAt(i)!='-'&&s.charAt(i)!='+') 
+				i++;
+			if(minus==true) 
+			{
+				start--;//if its negative enter the minus to the string
+				this.add(new Monom(s.substring(start,i)));
+				minus=false;
+			}
+			else {
+				String sub=s.substring(start,i);
+				Monom m=new Monom(sub);
+				this.add(m);
+			}
+			
+			if(i<s.length()&&s.charAt(i)!='-')
+				i++;
+		}
+	}
+	/**
+	 * @return the value of this polynom in the given x
+	 * @param int x
+	 */
+	public double f(double x) 
+	{
+		// The method return the result of x in the Polynom
+		double count=0;
+		for(int i=0;i<poly.size();i++) 
+		{
+			count=count+poly.get(i).f(x);
+		}
+		return count;
+	}
+	/**
+	 * @return if this Polynom contain the power p1
+	 * @param int p1
+	 */
+	private int containsPower(int p1)
+	{
+		/* The method return the place of the power in the array if it is already
+		in the polynom,else return -1*/
+		int count=0;
+		for(int i=0;i<poly.size();i++) 
+		{
+			if(poly.get(i).get_power()==p1)
+					return count;
+			count++;
+		}
+		return -1;
+	}
+	/**
+	 * Add Monom m1 to this Polynom
+	 * @param Monom m1
+	 */
+	public void add(Monom m1)
+	{
+		// The method add the Monom to the Polynom
+		int exist=this.containsPower(m1.get_power());
+		if(exist==(-1))//if the Polynom isn't contain the power of m1
+		{
+			if(!m1.equals(ZERO)) 
+			{
+				poly.add(m1);
+				//numOfMonoms++;
+			}
+		}
+		else 
+		{//add Monom m1 to the Monom that matching in the Polynom
+			poly.get(exist).add(m1);
+			if(poly.get(exist).equals(ZERO))//if the Monom is zero remove him from the array
+			{
+				poly.remove(exist);
+			//	numOfMonoms--;
+			}
+		}
+	}
+	/**
+	 * Add Polynom_able p1 to this Polynom
+	 * @param Polynom_able p1
+	 */
+	public void add(Polynom_able p1)
+	{
+		// The method add the Monom to the Polynom
+		
+		Iterator<Monom> iter1=p1.iteretor();
+		while(iter1.hasNext())	
+		{
+			this.add(iter1.next());
+		}
+	}
+	/**
+	 * Subtract Polynom_able p1 from this Polynom
+	 * @param Polynom_able p1
+	 */
+	public void substract(Polynom_able p1)
+	{	// The method subtract the p1 from the Polynom
+		Polynom_able copy=p1.copy();
+		copy.multiply(MINUS1);
+		this.add(copy);
+	}
+	/**
+	 * Multiply this Polynom by Monom m1
+	 * @param Monom m1
+	 */
+	public void multiply(Monom m1)
+	{
+		// The method multiply the Polynom with m1
+		
+		if(m1.equals(ZERO)) //if Monoms is zero initialize the polynom
+		{
+			for(int i=0;i<poly.size();) 
+			{
+				poly.remove(i);
+				//numOfMonoms--;
+			}
+		//	numOfMonoms=0;
+		}
+		else
+		{
+			for(int i=0;i<poly.size();i++)
+			{
+				Monom m=new Monom(poly.get(i));
+				m.multipy(m1);
+				if(m.equals(ZERO))//if the monom equals to 0 remove him from the array
+				{
+					poly.remove(i);
+					i--;
+				//	this.numOfMonoms--;
+				}
+				else
+					poly.set(i, m);
+			}
+		}
+	}
+	/**
+	 * Multiply this Polynom by Polynom_able p1
+	 * @param Polynom_able p1
+	 */
+	public void multiply(Polynom_able p1) 
+	{
+	//  The method multiply polynom with polynom
+		Polynom_able copy=p1.copy();//incase p1 us this polynom.
+		Polynom_able tempChange=this.copy();
+		Polynom_able saver=this.copy();
+		this.multiply(ZERO);
+		Iterator<Monom> iter1=copy.iteretor();
+		while(iter1.hasNext())	
+		{
+			tempChange.multiply(iter1.next());
+			this.add(tempChange);
+			tempChange=saver.copy();
+		}
+	}
+	/**
+	 * Test if this Polynom is logically equals to Polynom_able p1.
+	 * @param Polynom_able p1
+	 * @return true if this polynom represents the same function as Polynom_able p1
+	 */
+	public boolean equals(Polynom_able p1)
+	{
+		// The method return true if the Polynoms are equals
+		Iterator<Monom> iter1=p1.iteretor();
+		int count=0;
+		while(iter1.hasNext())	//calculate the number of the Monoms
+		{
+			iter1.next();
+			count++;
+		}
+		if(count!=poly.size()) //if the number different return false
+			return false;
+		iter1=p1.iteretor();
+		while(iter1.hasNext())	
+		{
+			Monom m=new Monom(iter1.next());
+			int power=this.containsPower(m.get_power());
+			if(power!=-1)
+			{
+				//if the power is the same check if the Monoms are equals
+				if(!m.equals(poly.get(power)))
+					return false;
+			}
+			else
+				return false;
+		}
+		return true;
+	}
+	/**
+	 * Test if this is the Zero Polynom
+	 * @return if the Polynom is zero
+	 */
+	public boolean isZero()
+	{
+		if(poly.size()==0)
+			return true;
+		return false;
+	}
+	/**
+	 * @return the root of this Polynom between x0 and x1.
+	 * @param double x0, double x1, double eps
+	 */
+	public double root(double x0, double x1, double eps) 
+	{
+		if(x0<=x1)
+		{
+			if (Math.abs(this.f(x0))<=eps)
+				return x0;
+			else if(Math.abs(this.f(x1))<=eps)
+				return x1;
+			double middleX=(x1+x0)/2;
+			double middleY=f((x1+x0)/2);
+			if(Math.abs(middleY)<=eps)
+				return middleX;
+			if(this.f(x0)>0&&this.f(x1)<0) 
+			{
+				if(middleY>0)
+					return this.root(middleX, x1, eps);
+				else
+					return this.root(x0, middleX, eps);
+			}			
+			else if(this.f(x0)<0&&this.f(x1)>0) 
+			{
+				if(middleY>0)
+					return this.root(x0, middleX, eps);
+				else
+					return this.root(middleX, x1, eps);
+			}		
+			else
+				throw new RuntimeException("ERR f(x0)*f(x1)>0 ");
+		}
+		else
+			throw new RuntimeException("ERR x0>x1 ");
+	}
+	/**
+	 * create a deep copy of this Polynom
+	 * @return the copy
+	 **/
+	public Polynom_able copy()
+	{
+		Polynom temp=new Polynom(this.toString());
+		return temp;
+	}
+	/**
+	 * Compute a new Polynom which is the derivative of this Polynom
+	 * @return the derivative
+	 */
+	public Polynom_able derivative() 
+	{
+		// The method return the derivative of the polynom
+		Polynom_able der=new Polynom();
+		for(int i=0;i<poly.size();i++) 
+		{
+			Monom m1=new Monom(this.poly.get(i).derivative());
+			der.add(m1);
+		}
+		return der;
+	}
+	/**
+	 * @return the area of this Polynom between x0 and x1
+	 * @param double x0, double x1, double eps
+	 */
+	public double area(double x0, double x1, double eps)
+	{
+		// The method calculate the area of the Polynom approximately of eps
+		double sum=0;
+		if(x0>=x1)
+		{
+			return 0;
+		}
+		while(x0<x1) 
+		{
+			if(f(x0)>0) 
+			{
+				sum=sum+eps*f(x0);
+			}
+			x0=x0+eps;
+		}
+		return sum;
+	}
+	/**
+	 * @return an Iterator (of Monoms) over this Polynom
+	 * 
+	 */
+	public Iterator<Monom> iteretor()
+	{
+		// Return an Iterator (of Monoms) over this Polynom
+		Iterator<Monom> iter1=poly.iterator();
+		return iter1;
+	}
+	/**
+	 * @return a string that represent this Polynom
+	 * 
+	 */
+	public String toString()
+	{
+		//The method print the Polynom
+		Collections.sort(poly,new Monom_Comperator());//sort the Polynom
+		if(poly.size()==0)
+			return "0";
+		String p=""+poly.get(0);
+		for(int i=1;i<poly.size();i++)
+		{
+			if(poly.get(i).get_coefficient()<0) 
+				p=p+poly.get(i);
+			else
+				p=p+"+"+poly.get(i);
+		}
+		return p;
+	}
+	@Override
+	public function initFromString(String s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+    
